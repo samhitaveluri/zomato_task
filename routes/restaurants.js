c7514d661dd49095e3595eb961fccb1e9e1edaa5
samhitaveluri <121224422+samhitaveluri@users.noreply.github.com>
2024-12-08 22:47:30 +0530

    Update restaurants.js

--- routes/restaurants.js
+++ routes/restaurants.js
@@ -1,17 +1,15 @@
 const express = require('express');
 const router = express.Router();
-const Restaurant = require('./models/Restaurant'); // Update this path to your Restaurant model
-
-// Search by name
+const Restaurant = require('./models/Restaurant');  
+ 
 router.get('/search', async (req, res) => {
   try {
     const { name } = req.query;
     if (!name) {
       return res.status(400).json({ error: 'Name query parameter is required' });
     }
-
     const restaurants = await Restaurant.find({
-      'Restaurant Name': { $regex: name, $options: 'i' } // Case-insensitive search
+      'Restaurant Name': { $regex: name, $options: 'i' } 
     });
 
     res.json(restaurants);
