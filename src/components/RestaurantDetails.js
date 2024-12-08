5b9b28239b347a2072a9c63b234d4c20aa0d58c9
samhitaveluri <121224422+samhitaveluri@users.noreply.github.com>
2024-12-08 22:44:33 +0530

    Update RestaurantDetails.js

--- src/components/RestaurantDetails.js
+++ src/components/RestaurantDetails.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import './RestaurantDetails.css';   
+import './Restaurant.css';   
 
 const RestaurantDetails = () => {
   const { id } = useParams();   
